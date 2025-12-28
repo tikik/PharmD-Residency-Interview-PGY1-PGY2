3de2de11841dbb3cd665a59725e6259abe688ada
Trang K <79597448+tikik@users.noreply.github.com>
2025-12-28 06:35:53 -0700

    Update ingest_pdfs.py

--- ingest_pdfs.py
+++ ingest_pdfs.py
@@ -2,7 +2,7 @@
 import os
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain-chroma import Chroma
+from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
 if not os.path.exists("data"):
