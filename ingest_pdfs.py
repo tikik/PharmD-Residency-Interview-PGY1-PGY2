70d633a125a55e98290e0f42d18835dc5e33025c
Trang K <79597448+tikik@users.noreply.github.com>
2025-12-29 21:23:41 -0700

    Update ingest_pdfs.py

--- ingest_pdfs.py
+++ ingest_pdfs.py
@@ -4,6 +4,8 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+import chromadb
+from chromadb.config import Settings
 
 if not os.path.exists("data"):
     os.makedirs("data")
